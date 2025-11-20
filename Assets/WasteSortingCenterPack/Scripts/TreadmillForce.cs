672267682e284e2b38ecce9d3909ce46df626f8e
benjamin333sz <b3szurek@gmail.com>
2025-11-20 10:10:19 +0100

    feat: small change

--- Assets/WasteSortingCenterPack/Scripts/TreadmillForce.cs
+++ Assets/WasteSortingCenterPack/Scripts/TreadmillForce.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TreadmillForce : MonoBehaviour
 {
     public float currentSpeed { get; private set; }
 
+    public float GetSpeed() {return currentSpeed;}
     private void OnTriggerStay(Collider other)
     {
         Rigidbody body = other.attachedRigidbody;
